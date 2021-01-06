0242ee19f771036b0636e8b522d3922e98068806
罗旭宏 <barntet@163.com>
2021-01-06 20:36:06 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,11 @@
 module.exports = {
   title: "笔记",
-  description: "barnett的blog",
+  base: "/",
   themeConfig: {
     sidebar: [
       {
-        title: "Js", // 必要的
-        path: "/noteBooks/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1, // 可选的, 默认值是 1
+        title: "Js",
+        initialOpenGroupIndex: -1,
         children: [
           "noteBooks/Promise.md",
           "noteBooks/Async.md",
@@ -17,6 +15,10 @@ module.exports = {
         ],
       },
     ],
-    nav: [{ text: "笔记", link: "/noteBooks/" }],
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "JS", link: "/noteBooks/Promise.md" },
+      { text: "Github", link: "https://github.com/barntet" },
+    ],
   },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
-heroImage: /assets/img/es6.png
-heroText: 1
-tagline: 22
+heroImage: /es6.jpg
+heroText: 不积跬步无以至千里
+tagline: go
 actionText: 快速开始 →
-actionLink: /noteBooks/
+actionLink: /noteBooks/Promise.md
 features:
   - title: 简洁至上
     details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
